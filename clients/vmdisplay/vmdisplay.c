bccd17c58feafe1520ea7a295defff9f35d3c329
Liu Xinyun <xinyun.liu@intel.com>
2019-06-18 15:28:36 +0800

    dw's fix close fd

--- clients/vmdisplay/vmdisplay.c
+++ clients/vmdisplay/vmdisplay.c
@@ -451,8 +451,7 @@ static void create_new_buffer_common(int dmabuf_fd)
 					current_texture_sampler_format =
 					    DRM_FORMAT_ARGB8888;
 				} else {
-					printf("DRI not supporint NV12\n");
-					close(dmabuf_fd);
+					printf("DRI not supporing NV12\n");
 					return;
 				}
 			}
@@ -545,7 +544,6 @@ static void create_new_buffer_common(int dmabuf_fd)
 							    surf_format, 0);
 		current_buffer = buf;
 	}
-	close(dmabuf_fd);
 	update_oldest_rec_hyper_dmabuf(hyper_dmabuf_id.id, textureId, buf,
 				       surf_width, surf_height, 0);
 }
@@ -566,6 +564,8 @@ void create_new_hyper_dmabuf_buffer(void)
 	}
 
 	create_new_buffer_common(msg.fd);
+
+	close(msg.fd);
 }
 
 void clear_hyper_dmabuf_list(void)
